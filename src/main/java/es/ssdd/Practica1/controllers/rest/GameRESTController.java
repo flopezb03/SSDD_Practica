3ad8d391036c6d142ab96771066f6febe44bbcd2
ferloz97 <fernando97.lbgr@gmail.com>
2024-03-08 13:11:59 +0100

    Game entity, service, restController

---
+++ src/main/java/es/ssdd/Practica1/controllers/rest/GameRESTController.java
@@ -0,0 +1,54 @@
+package es.ssdd.Practica1.controllers.rest;
+
+import es.ssdd.Practica1.entities.Game;
+import es.ssdd.Practica1.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RequestMapping("/api/")
+@RestController
+public class GameRESTController {
+
+    @Autowired
+    private GameService gameService;
+
+    @PostMapping("games")
+    public ResponseEntity<Game> createGame(@RequestBody Game game){
+        return ResponseEntity.status(201).body(gameService.createGame(game));
+    }
+    @GetMapping("games/{id}")
+    public ResponseEntity<Game> readGame(@PathVariable long id){
+        Game game = gameService.readGame(id);
+        if(game == null)
+            return ResponseEntity.status(404).build();
+        return ResponseEntity.status(200).body(game);
+    }
+    @PutMapping("games/{id}")
+    public ResponseEntity<Game> updateGame(@PathVariable long id, @RequestBody Game updatedGame){
+        Game game = gameService.updateGame(id,updatedGame);
+        if(game == null)
+            return ResponseEntity.status(404).build();
+        return ResponseEntity.status(200).body(game);
+    }
+    @DeleteMapping("games/{id}")
+    public ResponseEntity<Game> deleteGame(@PathVariable long id){
+        Game game = gameService.deleteGame(id);
+        if(game == null)
+            return ResponseEntity.status(404).build();
+        return ResponseEntity.status(200).build();
+    }
+    @PatchMapping("games/{id}")
+    public ResponseEntity<Game> patchGame(@PathVariable long id, @RequestBody Game updatedGame){
+        Game game = gameService.patchGame(id,updatedGame);
+        if(game == null)
+            return ResponseEntity.status(404).build();
+        return ResponseEntity.status(200).body(game);
+    }
+    @GetMapping("games")
+    public ResponseEntity<Collection<Game>> getAllGame(){
+        return ResponseEntity.status(200).body(gameService.getAllGame());
+    }
+}
