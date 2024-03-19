13ae1cb1833baa187107c261e96fa706e3014b20
Sirmagnetone <javierrg11.jrg@gmail.com>
2024-03-19 20:02:52 +0100

    style.css moved to css folder. errorStyle.css added

--- src/main/java/es/ssdd/Practica1/controllers/web/mainMenuController.java
+++ src/main/java/es/ssdd/Practica1/controllers/web/mainMenuController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class mainMenuController {
-    @GetMapping
+    @GetMapping("/startMenu")
     public String getMainMenu(){
         return "mainMenu";
     }
