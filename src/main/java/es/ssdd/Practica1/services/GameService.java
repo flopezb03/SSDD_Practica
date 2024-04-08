5661795efda7abcc0470222e0799c6a66037f3fd
ferloz97 <fernando97.lbgr@gmail.com>
2024-04-08 17:40:09 +0200

    Game Service modified

--- src/main/java/es/ssdd/Practica1/services/GameService.java
+++ src/main/java/es/ssdd/Practica1/services/GameService.java
@@ -1,45 +1,77 @@
 package es.ssdd.Practica1.services;
 
 import es.ssdd.Practica1.entities.Game;
+import es.ssdd.Practica1.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class GameService {
 
-    private final Map<Long, Game> games= new HashMap();
-    private final AtomicLong nextId = new AtomicLong();
+    @Autowired
+    private GameRepository gameRepository;
+    //private final Map<Long, Game> games= new HashMap();
+    //private final AtomicLong nextId = new AtomicLong();
 
 
 
     public Game createGame(Game game){
-        long id = nextId.getAndIncrement();
+        return gameRepository.save(game);
+        /*long id = nextId.getAndIncrement();
         game.setId(id);
         games.put(id,game);
-        return game;
+        return game;*/
     }
     public Game getGame(long id){
-        return games.get(id);
+        Optional<Game> byId = gameRepository.findById(id);
+        return byId.orElse(null);
+        //return games.get(id);
     }
     public Game putGame(long id, Game updatedGame){
-        Game oldGame = games.get(id);
+        Optional<Game> byId = gameRepository.findById(id);
+        if(byId.isPresent()) {
+            updatedGame.setId(id);
+            return gameRepository.save(updatedGame);
+        }
+        return null;
+        /*Game oldGame = games.get(id);
         if(oldGame == null)
             return null;
         updatedGame.setId(id);
         games.put(id,updatedGame);
-        return updatedGame;
+        return updatedGame;*/
     }
     public Game deleteGame(long id){
-        if(games.containsKey(id))
-            return games.remove(id);
+        Optional<Game> byId = gameRepository.findById(id);
+        if(byId.isPresent()) {
+            Game game = byId.get();
+            gameRepository.deleteById(id);
+            return game;
+        }
         return null;
+        /*if(games.containsKey(id))
+            return games.remove(id);
+        return null;*/
     }
     public Game patchGame(long id, Game updatedGame){
-        Game game = games.get(id);
+        Optional<Game> gameOp = gameRepository.findById(id);
+        if(gameOp.isEmpty())
+            return null;
+        Game game = gameOp.get();
+        if(updatedGame.getName() != null)
+            game.setName(updatedGame.getName());
+        if(updatedGame.getReleaseYear() != null)
+            game.setReleaseYear(updatedGame.getReleaseYear());
+        if(updatedGame.getDuration() != null)
+            game.setDuration(updatedGame.getDuration());
+        return gameRepository.save(game);
+        /*Game game = games.get(id);
         if(game == null)
             return null;
         if(updatedGame.getName() != null)
@@ -48,10 +80,11 @@ public class GameService {
             game.setReleaseYear(updatedGame.getReleaseYear());
         if(updatedGame.getDuration() != null)
             game.setDuration(updatedGame.getDuration());
-        return game;
+        return game;*/
 
     }
     public Collection<Game> getAllGame(){
-        return games.values();
+        return gameRepository.findAll();
+        //return games.values();
     }
 }
